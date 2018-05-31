8e5b3622ec02259e981850b61b7e5a38a3ab6d0c
ConstanzaGarcia <o.conii.o@gmail.com>
2018-05-31 15:36:54 -0400

    trabajando en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,29 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
-    var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-						'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-}
+
+    const contador= mensaje.length;
+
+    let ABC= 0;
+
+    let x= 
+      for (let i = 0; i < contador; i++);
+
+      abc = mensaje.charCodeAt(i);  /* le da los numeros a las letras, aqui llego 1era letras del mensaje */
+
+      x = (posicion - 65 -objet)%26 + 65; 
+
+    
+
+
+
+      
+      {
+        console.log(str[i]);
+        ('esta es la interacción número: ' + i + ', letra: ' + str [i]);
+        result += (str.charCodeAt (i) - 65 + 33)%26 + 65;[i].toLocaleLowerCase ();
+      }
+
+
+        }
+       }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 /* Acá va tu código */
 window.onload = function () {
+    /* Llamando al numero de intervalo, llamando al boton uno que traera el mensaje */
+    
+    const intervalo = document.getElementById ("intervalo"); 
+    const mensaje_uno = document.getElementById ("botonuno");
+
+
+
+    const mensaje_dos = document.getElementById ("botondos");
 
 }
