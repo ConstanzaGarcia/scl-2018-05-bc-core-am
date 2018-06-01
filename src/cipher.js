5644a11e089c996161895d7bbe0b197a226af06a
ConstanzaGarcia <o.conii.o@gmail.com>
2018-06-01 01:30:45 -0400

    CSS identación de dos espacios

--- src/cipher.js
+++ src/cipher.js
@@ -2,18 +2,29 @@ window.cipher = {
   encode: () => {
     /* Acá va tu código */
 
-    const contador= mensaje.length;
-
+    const contador= mensaje_uno.length;
+    /* mensajeuno*/
+    const mensaje_dos = "";
+    let letra = "";
     let ABC= 0;
+    let i;
+    let x;
 
-    let x= 
-      for (let i = 0; i < contador; i++);
+    for (i = 0; i < contador; i++)
+  {
+      letra = mensaje_uno[i]
+      result += (mensaje_uno.charCodeAt (i) - 65 + 33)%26 + 65;[i].toLocaleLowerCase ();
+      mensaje_dos [i] = result
+    }
 
+/*    
+      for (i = 0; i < contador; i++);
+        mensaje_uno [i] = 
       abc = mensaje.charCodeAt(i);  /* le da los numeros a las letras, aqui llego 1era letras del mensaje */
-
+/*
       x = (posicion - 65 -objet)%26 + 65; 
 
-    
+ 
 
 
 
@@ -26,4 +37,4 @@ window.cipher = {
 
 
         }
-       }
+       } 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 /* Acá va tu código */
 window.onload = function () {
     /* Llamando al numero de intervalo, llamando al boton uno que traera el mensaje */
-    
-    const intervalo = document.getElementById ("intervalo"); 
-    const mensaje_uno = document.getElementById ("botonuno");
-
-
-
-    const mensaje_dos = document.getElementById ("botondos");
-
-}
+     
+    const intervalo = document.getElementById ("intervalo").addEventListener('click', () =>{
+    let mensaje_uno = document.getElementById ("cifrar").value;
+    let botonouno= document.getElementById('textIn').value;
+    alert (cifrar);
+    document.getElementById('cajauno').innerHTML = result; })}
