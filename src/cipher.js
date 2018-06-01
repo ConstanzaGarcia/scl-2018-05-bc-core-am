5e8ca90a8dadb6e14aa069847d5659b7255581bc
ConstanzaGarcia <o.conii.o@gmail.com>
2018-06-01 11:41:27 -0400

    encode listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,54 +1,16 @@
 window.cipher = {     /* Acá va tu código */
   encode: (text,intervalo) => {
     let result = " ";
-      for (i = 0; i < contador.length; i++) {
+      for (i = 0; i < text.length; i++) {
 
       let intervalo = (text.charCodeAt (i) - 65) - parseInt(intervalo) % 26+65;
       
-      if (text.charCodeAt(i) === 32{
+      if (text.charCodeAt(i) === 32) {
         intervalo = "";
         result += "";
       }
-      result += string.fromcharCodeA (intervalo) === 32) {
-        outputAscii = " ";
-        result += " ";
-      }
-
-      result+= string.fromCharCode (intervalo)
-     }
-     return result;
-  {
-
-
-
-
-
-      letra = mensaje_uno[i]
-      result += (mensaje_uno.charCodeAt (i) - 65 + 33)%26 + 65;[i].toLocaleLowerCase ();
-      mensaje_dos [i] = result
+      result += String.fromCharCode("intervalo");
     }
-
-    return result
-/*    
-      for (i = 0; i < contador; i++);
-        mensaje_uno [i] = 
-      abc = mensaje.charCodeAt(i);  /* le da los numeros a las letras, aqui llego 1era letras del mensaje */
-/*
-      x = (posicion - 65 -objet)%26 + 65; 
-
- 
-
-
-
-      
-      {
-        console.log(str[i]);
-        ('esta es la interacción número: ' + i + ', letra: ' + str [i]);
-        result += (str.charCodeAt (i) - 65 + 33)%26 + 65;[i].toLocaleLowerCase ();
-      }
-
-
-    
-       */
-    } 
+    return result;
   }
+ } 
