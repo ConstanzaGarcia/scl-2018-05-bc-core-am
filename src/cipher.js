d4b3eebba78b922b09580b2b65b53877639a1294
ConstanzaGarcia <o.conii.o@gmail.com>
2018-06-17 22:07:10 -0400

    Trabajando encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,26 @@
 window.cipher = {     /* Acá va tu código */
-  encode: (text,intervalo) => {
-    let result = " ";
-      for (i = 0; i < text.length; i++) {
+  encode: (text, intervalo) => { /*Texto corresponde al lo que se va ingresar en el input (son dos argumentos)*/
 
-      let intervalo = (text.charCodeAt (i) - 65) - parseInt(intervalo) % 26+65;
-      
-      if (text.charCodeAt(i) === 32) {
-        intervalo = "";
-        result += "";
+    let result = ' '; /* string vacio que corresponde a lo que va ingresar en el usuario*/
+
+    /*el for va a recorrer el texto y va a tomar la longitud con el punto net*/
+    for (let i = 0; i < text.length; i++) {
+
+      let ascii = text.charCodeAt(i); /* obtiene el valor ascii*/
+      /* que la variable que buscal al codigo ascii es 32 el str vacio sume un espacio*/
+      if (ascii === 32) {
+        result += ' ';
       }
-      result += String.fromCharCode("intervalo");
+
+      /* va en doble parentesis porque va evaluando dos cosas- tienen que ser ambas condiciones verdaderas para que tome lo demás*/
+      if ((ascii >= 65) && (ascii <= 90)) {
+
+        /*toma el valor de ascii y lo pasa a las letras a traves del string */
+        let cifrar = (ascii - 65 + parseInt(intervalo)) % 26 + 65;
+
+        /* ese string vacio le esta como resultado la letra de arriba*/
+        result += String.fromCharCode(cifrar);
+
+      }
     }
-    return result;
-  }
- } 
+    return result }}
