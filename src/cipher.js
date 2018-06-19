934beb08498fb0a65f794f9d0334d305f1bd1fb6
ConstanzaGarcia <o.conii.o@gmail.com>
2018-06-19 17:53:59 -0400

    cifrado y decifrado, falta correr un test

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 window.cipher = {     /* Acá va tu código */
   encode: (text, intervalo) => { /*Texto corresponde al lo que se va ingresar en el input (son dos argumentos)*/
 
-    let result = ' '; /* string vacio que corresponde a lo que va ingresar en el usuario*/
+    let result = ''; /* string vacio que corresponde a lo que va ingresar en el usuario*/
 
     /*el for va a recorrer el texto y va a tomar la longitud con el punto net*/
     for (let i = 0; i < text.length; i++) {
@@ -20,7 +20,36 @@ window.cipher = {     /* Acá va tu código */
 
         /* ese string vacio le esta como resultado la letra de arriba*/
         result += String.fromCharCode(cifrar);
-
       }
+
+      if ((ascii >= 97) && (ascii <= 122)) { //con minusculas en ascii
+        let cifrar = (ascii - 65 + parseInt(intervalo)) % 26 + 65;
+        result += String.fromCharCode(cifrar);
+      }
+
     }
-    return result }}
+    return result;
+
+  },
+
+  decode: (textodos, intervalo) => {
+    let result2 = '';
+    for (let i = 0; i < textodos.length; i++) {
+      let asciidos = textodos.charCodeAt(i);
+      if (asciidos === 32) {
+        result2 += ' ';
+      }
+      if ((asciidos >= 65) && (asciidos <= 90)) {
+        let decifrar = (asciidos - 90 + parseInt(intervalo)) % 26 + 90;
+        result2 += String.fromCharCode(decifrar);
+      }
+      if ((asciidos >= 97) && (asciidos <= 122)) { //con minusculas en ascii
+        let decifrar = (ascii - 122 + parseInt(intervalo)) % 26 + 122;
+        result2 += String.fromCharCode(decifrar);
+      }
+
+    }
+
+    return result2;
+  },
+}
