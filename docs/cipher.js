f3a958b1af3640ace57454a26f6e058c12ce58ec
ConstanzaGarcia <o.conii.o@gmail.com>
2018-06-26 10:47:49 -0400

    actualizando carpeta docs

--- docs/cipher.js
+++ docs/cipher.js
@@ -1,16 +1,55 @@
 window.cipher = {     /* Acá va tu código */
-  encode: (text,intervalo) => {
-    let result = " ";
-      for (i = 0; i < text.length; i++) {
+  encode: (text, intervalo) => { /*Texto corresponde al lo que se va ingresar en el input (son dos argumentos)*/
 
-      let intervalo = (text.charCodeAt (i) - 65) - parseInt(intervalo) % 26+65;
-      
-      if (text.charCodeAt(i) === 32) {
-        intervalo = "";
-        result += "";
+    let result = ''; /*string vacio que corresponde a lo que va ingresar en el usuario*/
+
+    /*el for va a recorrer el texto y va a tomar la longitud con el punto length*/
+    for (let i = 0; i < text.length; i++) {
+
+      let ascii = text.charCodeAt(i); /* obtiene el valor ascii*/
+      /* la variable que busca, el codigo ascii es 32, el str vacio sume un espacio*/
+      if (ascii === 32) {
+        result += ' ';
       }
-      result += String.fromCharCode("intervalo");
+
+      /* va en doble parentesis porque va evaluando dos cosas- tienen que ser ambas condiciones verdaderas para que tome lo demás*/
+      if ((ascii >= 65) && (ascii <= 90)) {
+
+        /*toma el valor de ascii y lo pasa a las letras a traves del string */
+        let cifrar = (ascii - 65 + parseInt(intervalo)) % 26 + 65;
+
+        /* ese string vacio le esta como resultado la letra de arriba*/
+        result += String.fromCharCode(cifrar);
+      }
+
+      if ((ascii >= 97) && (ascii <= 122)) { //con minusculas en ascii
+        let cifrar = (ascii - 65 + parseInt(intervalo)) % 26 + 65;
+        result += String.fromCharCode(cifrar);
+      }
+
     }
     return result;
-  }
- } 
+
+  },
+
+  decode: (textodos, intervalo) => {
+    let result2 = '';
+    for (let i = 0; i < textodos.length; i++) {
+      let asciidos = textodos.charCodeAt(i);
+      if (asciidos === 32) {
+        result2 += ' ';
+      }
+      if ((asciidos >= 65) && (asciidos <= 90)) {
+        let decifrar = (asciidos - 90 - parseInt(intervalo)) % 26 + 90;
+        result2 += String.fromCharCode(decifrar);
+      }
+      if ((asciidos >= 97) && (asciidos <= 122)) {
+        let decifrar = (ascii - 122 - parseInt(intervalo)) % 26 + 122;
+        result2 += String.fromCharCode(decifrar);
+      }
+
+    }
+
+    return result2;
+  },
+}
--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,14 @@
-/* Acá va tu código */
-window.onload = function () {
-    /* Llamando al numero de intervalo-llamando al boton uno que traera el mensaje */
-     
-    let intervalo = document.getElementById ("intervalo");
-    let mensaje_uno = document.getElementById ("encode");
-    let mensaje_dos= document.getElementById('decode');
+/* Llamando al numero de intervalo-llamando al boton uno que traera el mensaje */
+const ascii = document.getElementById('botonuno').addEventListener('click', () => {
+    let intervalo = document.getElementById('intervalo').value;
+    let text = document.getElementById('encode').value;
+    let result = window.cipher.encode(text, intervalo);
+    document.getElementById('cajauno').innerHTML = result;
+})
 
-    document.getElementById('botonuno').addEventListener('click', function encode() {
-        document.getElementById('encode').innerHTML = window.cipher.encode(cajauno.value.toUpperCase(),
-          Number(offset.value));
-      })
-      
-      document.getElementById('botondos').addEventListener('click', function decode() {
-        document.getElementById('decode').innerHTML = window.cipher.decode(cajados.value.toUpperCase(),
-          Number(offset.value));
-      })
-
-}
+const asciidos = document.getElementById('botondos').addEventListener('click', () => {
+    let intervalo = document.getElementById('intervalo').value;
+    let textodos = document.getElementById('decode').value;
+    let result2 = window.cipher.decode(textodos, intervalo);
+    document.getElementById('cajados').innerHTML = result2;
+})
